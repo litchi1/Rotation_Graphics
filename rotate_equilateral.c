93fa8ffec90d6a15be9c4dbbd1db71e9d5c771d1
Dipankar Kaushik <litchicode@gmail.com>
2019-01-05 19:58:39 +0530

    Update rotate_equilateral.c

--- rotate_equilateral.c
+++ rotate_equilateral.c
@@ -4,8 +4,9 @@
 #define ROUND(a) ((int)(a + 0.5))
 #define PII 3.14
 
+/* Prototypes of the funtions used*/
 void triangle(int, int, int, int, int, int);
-void rotation(int, int, int, int, int, int, float);
+void rotation(int, int, int, int, int, int);
 void dda1(int, int, int, int);
 void dda2(int, int, int, int);
 void dda3(int, int, int, int);
@@ -14,17 +15,15 @@ int main()
 {
     int gd = DETECT, gm;
     int x1, y1, x2, y2, x3, y3;
-    initgraph(&gd, &gm, "");
+    initgraph(&gd, &gm, NULL);
 
-    printf("The coordinates of the equilateral triangle are (250, 114), (200, 200) and (300, 200) and its centroid is (250, 172)\n");
+    printf("The coordinates of the equilateral triangle are (395, 270), (320, 400) and (470, 400).\n\n");
 
-    triangle(250, 114, 200, 200, 300, 200);
+    triangle(395, 270, 320, 400, 470, 400);
     delay(1000);
 
-    printf("The angle of rotation is 45 degrees\n");
-
-    rotation(250, 114, 200, 200, 300, 200, -45); //negative angle because the origin starts from top in the graphics window frame
-    delay(5000);
+    rotation(395, 270, 320, 400, 470, 400);
+    getch();
 }
 
 /*Function to draw the triangle using dda algorithm*/
@@ -36,13 +35,21 @@ void triangle(int x1, int y1, int x2, int y2, int x3, int y3)
 }
 
 /*Function to rotate the triangle to specified angle*/
-void rotation(int x1, int y1, int x2, int y2, int x3, int y3, float theta)
+void rotation(int x1, int y1, int x2, int y2, int x3, int y3)
 {
-    int l1, m1, l2, m2, l3, m3, xf = 250, yf = 172;
+    int l1, m1, l2, m2, l3, m3, xf, yf;
 
-    /*cleardevice() to be used if original triangle not to be displayed. Remove the comments below to use this function*/
+    /* Calculating the coordinates of centroid of the equilateral triangle */
 
-    //cleardevice();
+    xf = (x1 + x2 + x3) / 3;
+    yf = (y1 + y2 + y3) / 3;
+
+    printf("The centroid of the equilateral triangle is (%d, %d).\n\n", xf, yf);
+
+    cleardevice();
+
+    float theta = -45;
+    printf("The angle of rotation is 45 degrees in anticlockwise direction.\n");
 
     /*Convert angle in degree to radian because trigonometric functions use angles in radian*/
     theta = (theta * PII) / 180;
